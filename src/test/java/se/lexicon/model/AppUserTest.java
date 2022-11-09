42fa358ee1af508c03d3acd0f04082695959e975
NiveSJ <niveathithavj@gmail.com>
2022-11-09 15:57:37 +0100

    updated tesst cases

--- src/test/java/se/lexicon/model/AppUserTest.java
+++ src/test/java/se/lexicon/model/AppUserTest.java
@@ -1,4 +1,37 @@
 package se.lexicon.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppUserTest {
+
+    AppUser appUser1 = new AppUser("Test", "Asdfg", AppRole.ROLE_APP_ADMIN);
+
+    @Test
+    public void appuser() {
+        String Username = appUser1.getUserName();
+        assertEquals(Username, "Test");
+
+        appUser1.setUserName("Test");
+        assertEquals(appUser1.getUserName(), "Test");
+
+
+        String Password = appUser1.getPassword();
+        assertEquals(Username, "Test");
+
+        appUser1.setPassword("Asdfg");
+        assertEquals(appUser1.getPassword(), "Asdfg");
+
+        AppRole app= appUser1.getAppRole();
+        assertEquals(AppRole.ROLE_APP_ADMIN, app);
+
+        appUser1.setAppRole(AppRole.ROLE_APP_ADMIN);
+        assertEquals(appUser1.getAppRole(),AppRole.ROLE_APP_ADMIN);
+
+
+
+    }
+
+
 }
