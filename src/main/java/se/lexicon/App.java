7dbc33185c0efa588b8411b3bfe14b5136d3b5d4
NiveSJ <niveathithavj@gmail.com>
2022-12-18 11:26:09 +0100

    added DBA Interfaces

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,16 +1,11 @@
 package se.lexicon;
 
-import se.lexicon.controller.Controller;
-import se.lexicon.exceptions.MyOwnRuntimeException;
-
 /**
  * Todo Assignment
  */
 public class App {
-    public static void main(String[] args) throws MyOwnRuntimeException {
+    public static void main(String[] args) {
 
-        Controller controller = new Controller();
-        controller.doMainMenu();
 
     }
 }
